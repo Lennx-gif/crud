60faded0bd5865251cc68a1043dff41946dd976e
Lenny Eugene <mysticanxtr8@gmail.com>
2025-02-24 03:37:01 +0300

    Add Axios for API calls and refactor Student component for data display

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+
 import "bootstrap/dist/css/bootstrap.min.css"
 //import 'bootstrap/dist/js/bootstrap'
 //import 'bootstrap/js/src/collapse'
--- src/student.js
+++ src/student.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react';
+import axios from 'axios';
+import  {data}  from 'react-router';
 
-function student() {
+function Student() {
+  const [Student,setStudent] = useState ([]);
+  useEffect(() => {
+    axios.get('http://localhost:8081/')
+    .then(res => setStudent(res.data))
+    .catch(err => console.log(err));
+  }, []);
   return (
-    <div>student</div>
-  )
-}
+    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+      <div className='w-50 bg-white rounded p-3'>
+        <button className='btn btn-success'>Add +</button>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+            
+          </thead>
 
-export default student
+          <tbody>
+            {
+              Student.map ((data,i) => (
+                <tr>
+                  <td>[data.ID]</td>
+                  <td>[data.Name]</td>
+                  <td>[data.Email]</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+
+      </div>
+    </div>
+  )
+};
+
+export default Student;
